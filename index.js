8fc3410cfbe142eca9ccff1d50dfb9ae09a58229
Angel Rodriguez <87674301+angelgoyo@users.noreply.github.com>
2024-02-13 16:08:10 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+// capturar body
+app.use(morgan("dev"));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors({ origin: "*" }));
+
+// Conexión a Base de datos
+require("./database");
+
+// import routes
+const authRoutes = require('./routes/auth');
+const dashboadRoutes = require('./routes/dashboard');
+const verifyToken = require('./routes/validate-token');
+
+// route middlewares
+app.use('/api/dashboard', verifyToken, dashboadRoutes);
+app.use('/api/user', authRoutes);
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funciona!'
+    })
+});
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+});
